a33532d7fc729a99f0499dcf417915c43a65b0ca
Justin <justin.d.lewis.mil@mail.mil>
2023-12-11 09:47:03 -0500

    Part 2.

--- 11/solve.py
+++ 11/solve.py
@@ -16,28 +16,24 @@ def universe_str(universe):
     return "\n".join("".join(row) for row in universe)
 
 
-def expand_universe(universe):
-    universe = expand_vertical(universe)
-    universe = expand_horizontal(universe)
-    return universe
+def expand_vertical(universe, expansion, coords):
+    empty_rows = np.argwhere((universe == "#").sum(axis=1) == 0).flatten()
+    for empty_row in empty_rows[::-1]:
+        for coord in coords:
+            if coord[0] > empty_row:
+                coord[0] += expansion
 
 
-def expand_vertical(universe):
-    rows = []
-    for row in universe:
-        rows.append(row)
-        if (row == "#").sum() == 0:
-            rows.append(row)
-    return np.array(rows)            
+def expand_horizontal(universe, expansion, coords):
+    empty_cols = np.argwhere((universe == "#").sum(axis=0) == 0).flatten()
+    for empty_col in empty_cols[::-1]:
+        for coord in coords:
+            if coord[1] > empty_col:
+                coord[1] += expansion
 
 
-def expand_horizontal(universe):
-    return expand_vertical(universe.T).T
-
-
-def get_distances(universe):
+def get_distances(galaxy_coords):
     distances = []
-    galaxy_coords = list(map(tuple, np.argwhere(universe == "#")))
     for idx, coord_0 in enumerate(galaxy_coords[:-1], start=1):
         for coord_1 in galaxy_coords[idx:]:
             distances.append(get_distance(coord_0, coord_1))
@@ -48,11 +44,19 @@ def get_distance(coord_0, coord_1):
     return abs(coord_1[0] - coord_0[0]) + abs(coord_1[1] - coord_0[1])
 
 
+def get_coords(universe, expansion=1):
+    coords = list(map(list, np.argwhere(universe == "#")))
+    expand_horizontal(universe, expansion, coords)
+    expand_vertical(universe, expansion, coords)
+    return coords
+
+
 def main(filename="input.txt"):
     universe = parse_universe(read_file(filename))
-    universe = expand_universe(universe)
-    print(f"part 1:  {sum(get_distances(universe))}")
-    print(f"part 2:  {None}")
+    print(f"expansion 1:  {sum(get_distances(get_coords(universe)))}")
+    print(f"expansion 10:  {sum(get_distances(get_coords(universe, 9)))}")
+    print(f"expansion 100:  {sum(get_distances(get_coords(universe, 99)))}")
+    print(f"expansion 1000000:  {sum(get_distances(get_coords(universe, 999999)))}")
 
 
 if __name__ == "__main__":
