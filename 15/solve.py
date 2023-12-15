93a06cf197ee50dcecadbaa0d2864757b3ac8782
Justin <18606523+jlewis200@users.noreply.github.com>
2023-12-15 08:11:02 -0500

    Part 2.

--- 15/solve.py
+++ 15/solve.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 
 def read_file(filename):
     with open(filename, encoding="utf-8") as f_in:
@@ -25,10 +27,30 @@ def hash_char(char, value):
     return ((value + ord(char)) * 17) % 256
 
 
+def get_focus_powers(boxes):
+    powers = []
+    for box, lenses in enumerate(boxes, start=1):
+        for position, lens in enumerate(lenses.values(), start=1):
+            powers.append(box * position * lens)
+    return powers
+
+
+def get_boxes(instructions):
+    boxes = [{} for idx in range(256)]
+    for instruction in instructions:
+        label, op, lens = re.match(r"([a-z]+)([=,-])(\d*)", instruction).groups()
+        box = boxes[hash_chunk(label)]
+        if op == "=":
+            box[label] = int(lens)
+        else:
+            box.pop(label, None)
+    return boxes
+
+
 def main(filename="input.txt"):
     instructions = parse_instructions(read_file(filename))
     print(f"part 1:  {sum(hash_iterable(instructions))}")
-    print(f"part 2:  {None}")
+    print(f"part 2:  {sum(get_focus_powers(get_boxes(instructions)))}")
 
 
 if __name__ == "__main__":
